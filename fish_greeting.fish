7f83556322d1b81f3346afd1af657bea7262ddb9
Dietrich Daroch <dietr1ch@acm.org>
2017-09-24 09:34:40 -0700

    Video card on warning

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -2,5 +2,11 @@ function fish_greeting -d "what's up, fish?"
   set_color $fish_color_autosuggestion
   uname -nmsr
   uptime
+  if test -e /proc/acpi/bbswitch
+    if grep "ON" /proc/acpi/bbswitch > /dev/null
+      set_color red
+      echo "Video card is ON!!"
+    end
+  end
   set_color normal
 end
